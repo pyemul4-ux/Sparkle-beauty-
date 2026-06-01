bf7c7bb2a1ded74d98aeca8510a13a5afa2a56cc
pyemul4-ux <pyemul4@gmail.com>
2026-06-01 18:58:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById("whatsapp-form").addEventListener("submit", function(e){
+
+e.preventDefault();
+
+let name = document.getElementById("name").value;
+
+let phone = document.getElementById("phone").value;
+
+let service = document.getElementById("service").value;
+
+let message = document.getElementById("message").value;
+
+let whatsappMessage =
+`Hello Sparkle Beauty,
+
+New Appointment Booking
+
+Name: ${name}
+
+Phone: ${phone}
+
+Service: ${service}
+
+Message: ${message}`;
+
+let url =
+"https://wa.me/919420713845?text=" + encodeURIComponent(whatsappMessage);
+
+window.open(url, "_blank");
+
+});
